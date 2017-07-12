c7dabd3f6ec8a777e5ce2270fe8b8ba3cfd97621
Kirill Volkovich <volkovich@scand.com>
2017-07-12 12:31:18 +0300

    Fix gh-pages build

--- gh-pages-update.sh
+++ gh-pages-update.sh
@@ -3,7 +3,6 @@
 rm -rf .gh-pages-tmp lib  &&
 mkdir .gh-pages-tmp &&
 
-showroom-scan src &&
 node node_modules/webpack/bin/webpack.js --config ./config/doc/webpack.config.js --hide-modules &&
 cp -R lib/* .gh-pages-tmp &&
 cp -R www/index.html .gh-pages-tmp &&
