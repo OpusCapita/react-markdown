023c2139aa62ceef3bb5586420e59fded788928f
Kirill Volkovich <volkovich@scand.com>
2017-07-12 11:26:45 +0300

    Move showroom components scan from js file to package.json goals

--- gh-pages-update.sh
+++ gh-pages-update.sh
@@ -2,6 +2,8 @@
 
 rm -rf .gh-pages-tmp lib  &&
 mkdir .gh-pages-tmp &&
+
+showroom-scan src &&
 node node_modules/webpack/bin/webpack.js --config ./config/doc/webpack.config.js --hide-modules &&
 cp -R lib/* .gh-pages-tmp &&
 cp -R www/index.html .gh-pages-tmp &&
