82705f9f7754fe84bac9b62911de9cfc1ca1d25b
Kirill Volkovich <volkovich@scand.com>
2017-07-12 14:41:06 +0300

    Fix gh-pages build
    [ci skip]

--- gh-pages-update.sh
+++ gh-pages-update.sh
@@ -6,6 +6,7 @@ mkdir .gh-pages-tmp &&
 node node_modules/webpack/bin/webpack.js --config ./config/doc/webpack.config.js --hide-modules &&
 cp -R lib/* .gh-pages-tmp &&
 cp -R www/index.html .gh-pages-tmp &&
+cp -R .gitignore .gh-pages-tmp &&
 
 git checkout gh-pages &&
 git ls-files | grep -v -e "\(^\.gitignore$\|^\.gitattributes$\|^\.gh-pages-tmp$\)" | xargs rm -rf &&
