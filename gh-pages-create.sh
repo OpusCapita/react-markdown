fe91a65a85d167c9479227c9dab2806a659a6422
Kirill Volkovich <volkovich@scand.com>
2017-07-14 10:15:17 +0300

    Update gh-pages-create.sh [ci skip]

--- gh-pages-create.sh
+++ gh-pages-create.sh
@@ -7,8 +7,6 @@ node node_modules/webpack/bin/webpack.js --config ./config/doc/webpack.config.js
 cp -R lib/* .gh-pages-tmp &&
 cp -R www/index.html .gh-pages-tmp &&
 cp -R .gitignore .gh-pages-tmp &&
-
-git branch -D gh-pages &&
-git checkout -b gh-pages &&
-git ls-files | grep -v -e "\(^\.gitignore$\|^\.gitattributes$\|^\.gh-pages-tmp$\)" | xargs rm -rf &&
 tar -cvzf demo.tar.gz .gh-pages-tmp
+
+
--- gh-pages-push.sh
+++ gh-pages-push.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+git branch -D gh-pages &&
+git checkout -b gh-pages &&
+git ls-files | grep -v -e "\(^\.gitignore$\|^\.gitattributes$\|^\.gh-pages-tmp$\)" | xargs rm -rf
 mv .gh-pages-tmp/* . &&
 rm -rf .gh-pages-tmp &&
 git add . &&
