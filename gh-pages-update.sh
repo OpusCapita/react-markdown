5f3bede7e98ef71592f277d8c6715cd21a499726
Kirill Volkovich <volkovich@scand.com>
2017-07-14 09:19:25 +0300

    Add creating archive with executable demo [ci skip]

--- gh-pages-update.sh
+++ gh-pages-update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -rf .gh-pages-tmp lib  &&
+rm -rf .gh-pages-tmp lib demo.tar.gz  &&
 mkdir .gh-pages-tmp &&
 
 node node_modules/webpack/bin/webpack.js --config ./config/doc/webpack.config.js --hide-modules &&
@@ -8,9 +8,10 @@ cp -R lib/* .gh-pages-tmp &&
 cp -R www/index.html .gh-pages-tmp &&
 cp -R .gitignore .gh-pages-tmp &&
 
-git branch -d gh-pages &&
+git branch -D gh-pages &&
 git checkout -b gh-pages &&
 git ls-files | grep -v -e "\(^\.gitignore$\|^\.gitattributes$\|^\.gh-pages-tmp$\)" | xargs rm -rf &&
+tar -cvzf demo.tar.gz .gh-pages-tmp &&
 mv .gh-pages-tmp/* . &&
 rm -rf .gh-pages-tmp &&
 git add . &&
