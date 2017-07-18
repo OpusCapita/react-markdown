e204452b50d681a10dcfc49567a5955147bd9a02
Kirill Volkovich <volkovich@scand.com>
2017-07-18 20:51:38 +0300

    Improve highlighter tests

--- src/client/components/SlateEditor/plugins/slate-markdown-plugin/highlighter.spec.js
+++ src/client/components/SlateEditor/plugins/slate-markdown-plugin/highlighter.spec.js
@@ -1,6 +1,7 @@
 import Prism from 'prismjs';
 import { grammar } from './MarkdownPreviewSchema';
 import { assert } from 'chai';
+import $ from 'cheerio';
 
 let allTypesEqual = (type, str, grammar) => {
   let tokens = Prism.tokenize(str, grammar);
@@ -22,6 +23,11 @@ let typeNotEqual = (type, str, grammar) => {
   return tokens.every(token => token.type !== type);
 };
 
+let getHtml = (str) => Prism.highlight(str, grammar);
+let escapeRegExp= function(str) {
+    return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
+
 describe('highlighter', () => {
   // it('should highlight blockquote', ()=> {
 
@@ -32,99 +38,108 @@ describe('highlighter', () => {
   // });
 
   it('should highlight header1', ()=> {
-    assert.isTrue(typeEqual('header1', '# Header header header', grammar));
-    assert.isTrue(typeNotEqual('header1', '#Header header header', grammar));
+    let html;
+
+    html = '# Header';
+    assert.equal(getHtml(html), '<span class="token header1"># Header</span>');
+
+    html = '#Header';
+    assert.equal(getHtml(html), '#Header');
   });
 
   it('should highlight header2', ()=> {
-    assert.isTrue(typeEqual('header2', '## Header header header', grammar));
-    assert.isTrue(typeNotEqual('header2', '##Header header header', grammar));
+    let html;
+
+    html = '## Header';
+    assert.equal(getHtml(html), '<span class="token header2">## Header</span>');
+
+    html = '##Header';
+    assert.equal(getHtml(html), '##Header');
   });
 
   it('should highlight header3', ()=> {
-    assert.isTrue(typeEqual('header3', '### Header header header', grammar));
-    assert.isTrue(typeNotEqual('header3', '###Header header header', grammar));
+    let html;
+
+    html = '### Header';
+    assert.equal(getHtml(html), '<span class="token header3">### Header</span>');
+
+    html = '###Header';
+    assert.equal(getHtml(html), '###Header');
   });
 
   it('should highlight header4', ()=> {
-    assert.isTrue(typeEqual('header4', '#### Header header header', grammar));
-    assert.isTrue(typeNotEqual('header4', '####Header header header', grammar));
+    let html;
+
+    html = '#### Header';
+    assert.equal(getHtml(html), '<span class="token header4">#### Header</span>');
+
+    html = '####Header';
+    assert.equal(getHtml(html), '####Header');
   });
 
   it('should highlight header5', ()=> {
-    assert.isTrue(typeEqual('header5', '##### Header header header', grammar));
-    assert.isTrue(typeNotEqual('header5', '#####Header header header', grammar));
+    let html;
+    html = '##### Header';
+    assert.equal(getHtml(html), '<span class="token header5">##### Header</span>');
+
+    html = '#####Header';
+    assert.equal(getHtml(html), '#####Header');
   });
 
   it('should highlight header6', ()=> {
-    assert.isTrue(typeEqual('header6', '###### Header header header', grammar));
-    assert.isTrue(typeNotEqual('header6', '######Header header header', grammar));
+    let html;
+
+    html = '###### Header';
+    assert.equal(getHtml(html), '<span class="token header6">###### Header</span>');
+
+    html = '######Header';
+    assert.equal(getHtml(html), '######Header');
   });
 
   it('should highlight hr', ()=> {
-    assert.isTrue(typeEqual(
-      'hr',
-      `
-      ***
-      some text
-      `,
-      grammar
-    ));
+    let html;
 
-    assert.isTrue(typeEqual(
-      'hr',
-      `
-      some text
-      ***
-      `,
-      grammar
-    ));
+    html = '---';
+    assert.equal(getHtml(html), '<span class="token hr">---</span>');
 
-    assert.isTrue(typeEqual(
-      'hr',
-      `
-      some text
-      ***
-      some text
-      `,
-      grammar
-    ));
+    html = '***';
+    assert.equal(getHtml(html), '<span class="token hr">***</span>');
 
-    assert.isTrue(typeEqual(
-      'hr',
-      `
-      ---
-      some text
-      `,
-      grammar
-    ));
+    html = '-----';
+    assert.equal(getHtml(html), '<span class="token hr">-----</span>');
 
-    assert.isTrue(typeEqual(
-      'hr',
-      `
-      some text
-      ---
-      `,
-      grammar
-    ));
+    html = '*****';
+    assert.equal(getHtml(html), '<span class="token hr">*****</span>');
 
-    assert.isTrue(typeEqual(
-      'hr',
-      `
-      some text
-      ---
-      some text
-      `,
-      grammar
-    ));
+    html = 'word-----';
+    assert.equal(getHtml(html), 'word-----');
 
-    assert.isTrue(typeNotEqual('hr', 'some text *** some text', grammar));
-    assert.isTrue(typeNotEqual('hr', 'some text --- some text', grammar));
+    html = 'word*****';
+    assert.equal(getHtml(html), 'word*****');
+
+    html = '-----word';
+    assert.equal(getHtml(html), '-----word');
+
+    html = '*****word';
+    assert.equal(getHtml(html), '*****word');
   });
 
   it('should highlight list', ()=> {
-    assert.isTrue(typeNotEqual('hr', '- asdf', grammar));
-    assert.isTrue(typeNotEqual('hr', 'some text - some tesxt', grammar));
+    let html;
+
+    html = '- item-1';
+    assert.equal(getHtml(html), '<span class="token list">- item-1</span>');
+
+    html = 'word - item-1';
+    assert.equal(getHtml(html), 'word - item-1');
+
+    html = '* item-1';
+    assert.equal(getHtml(html), '<span class="token list">* item-1</span>');
+
+    html = 'word * item-1';
+    assert.equal(getHtml(html), 'word * item-1');
+
+    // TODO - write more tests
   });
 
   // it('should highlight url', ()=> {
@@ -132,48 +147,99 @@ describe('highlighter', () => {
   // });
 
   it('should highlight bold', ()=> {
-    let str = '__bold__';
-    let tokens = Prism.tokenize(str, grammar);
-    assert.equal(tokens[0].type, 'bold');
+    let html;
 
-    // let str2 = '**bold**';
-    // let tokens2 = Prism.tokenize(str, grammar);
-    // assert.equal(tokens2[0].type, 'bold');
+    html = '__bold__';
+    assert.equal(getHtml(html), '<span class="token bold">__bold__</span>');
+
+    // html = '**bold**';
+    // assert.equal(getHtml(html), '<span class="token bold">**bold**</span>');
   });
 
   it('should highlight italic', ()=> {
-    let str = '_italic_';
-    let tokens = Prism.tokenize(str, grammar);
-    assert.equal(tokens[0].type, 'italic');
+    let html;
 
-    // let str2 = '*italic*';
-    // let tokens2 = Prism.tokenize(str2, grammar);
-    // assert.equal(tokens2[0].type, 'italic');
+    html = '_italic_';
+    assert.equal(getHtml(html), '<span class="token italic">_italic_</span>');
+
+    // html = '*italic*';
+    // assert.equal(getHtml(html), '<span class="token italic">*italic*</span>');
   });
 
-  it('should highlight bold-italic', ()=> {
-    let str = '___bold-italic___';
-    let tokens = Prism.tokenize(str, grammar);
+  it('should highlight bold-italic (underscores)', ()=> {
+    let html;
+    let candidate1;
+    let candidate2;
 
-    assert.equal(tokens[0].type, 'bold');
-    assert.equal(tokens[0].content[0].type, 'italic');
+    html = '___bold-italic___';
 
-    // let str2 = '***bold-italic***';
-    // let tokens2 = Prism.tokenize(str2, grammar);
+    candidate1 = escapeRegExp(
+      `<span class="token bold">__<span class="token italic">_bold-italic_</span>__</span>`
+    );
+    candidate2 = escapeRegExp(
+      `<span class="token italic">_<span class="token bold">__bold-italic__</span>_</span>`
+    );
 
-    // assert.equal(tokens2[0].type, 'bold');
-    // assert.equal(tokens2[0].content[0].type, 'italic');
+    assert.match(getHtml(html), new RegExp(`(${candidate1}|${candidate2})`));
   });
 
-  it('should highlight bold inside italic', ()=> {
-    let str = '___bold-italic__ italic_';
-    let tokens = Prism.tokenize(str, grammar);
+  it('should highlight bold-italic (asterisks)', ()=> {
+    let html;
+    let candidate1;
+    let candidate2;
 
-    assert.equal(tokens[0].content[1], ' italic_');
-    assert.equal(tokens[0].content[0].content, '___bold-italic__');
-    assert.equal(tokens[0].type, 'italic');
-    assert.equal(tokens[0].content[0].type, 'bold');
+    html = '***bold-italic***';
+
+    candidate1 = escapeRegExp(
+      `<span class="token bold">**<span class="token italic">*bold-italic*</span>**</span>`
+    );
+    candidate2 = escapeRegExp(
+      `<span class="token italic">*<span class="token bold">**bold-italic**</span>*</span>`
+    );
+
+    assert.match(getHtml(html), new RegExp(`(${candidate1}|${candidate2})`));
   });
+
+  it('should highlight bold-italic (underscores / asterisks mixed) v1', ()=> {
+    let html = '_**bold-italic**_';
+    let candidate = escapeRegExp(
+      `<span class="token italic">_<span class="token bold">**bold-italic**</span>_</span>`
+    );
+
+    assert.match(getHtml(html), new RegExp(candidate));
+
+  });
+
+  it('should highlight bold-italic (underscores / asterisks mixed) v2', ()=> {
+    let html = '*__bold-italic__*';
+    let candidate = escapeRegExp(
+      `<span class="token italic">*<span class="token bold">__bold-italic__</span>*</span>`
+    );
+
+    assert.match(getHtml(html), new RegExp(candidate));
+  });
+
+  it('should highlight bold-italic (underscores / asterisks mixed) v3', ()=> {
+    let html = '__*bold-italic*__';
+    let candidate = escapeRegExp(
+      `<span class="token bold">__<span class="token italic">*bold-italic*</span>__</span>`
+    );
+
+    assert.match(getHtml(html), new RegExp(candidate));
+  });
+
+  it('should highlight bold-italic (underscores / asterisks mixed) v4', ()=> {
+    let html = '**_bold-italic_**';
+    let candidate = escapeRegExp(
+      `<span class="token bold">**<span class="token italic">_bold-italic_</span>**</span>`
+    );
+
+    assert.match(getHtml(html), new RegExp(candidate));
+  });
+
+  // it('should highlight bold inside italic', ()=> {
+
+  // });
 
   // it('should highlight strikethrough', ()=> {
 
