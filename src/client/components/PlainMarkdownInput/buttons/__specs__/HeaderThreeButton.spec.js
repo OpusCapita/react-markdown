9fdef3e224f1d731ab3b56907466bda81a2627ee
Kirill Volkovich <volkovich@scand.com>
2017-08-16 14:35:42 +0300

    Repair tests

--- src/client/components/PlainMarkdownInput/buttons/__specs__/HeaderThreeButton.spec.js
+++ src/client/components/PlainMarkdownInput/buttons/__specs__/HeaderThreeButton.spec.js
@@ -5,29 +5,17 @@ import { Plain } from '@opuscapita/slate';
 import HeaderThreeButton from '../HeaderThreeButton';
 
 describe('<HeaderThreeButton/>', () => {
-  it('check active button', () => {
-    const state = Plain.deserialize("### some text");
-    const wrapper = shallow(<HeaderThreeButton state={state} onChange={() => {}} />);
-    expect(wrapper.find('.active')).to.have.length(1);
-  });
-
-  it('check inactive button', () => {
-    const state = Plain.deserialize("some text");
-    const wrapper = shallow(<HeaderThreeButton state={state} onChange={() => {}} />);
-    expect(wrapper.find('.active')).to.have.length(0);
-  });
-
   it('check unwrap header', () => {
     let state = Plain.deserialize("### some text");
     const wrapper = shallow(<HeaderThreeButton state={state} onChange={(newState) => { state = newState }} />);
-    wrapper.find('button').simulate('click');
+    wrapper.simulate('click');
     expect(Plain.serialize(state)).to.equal('some text');
   });
 
   it('check replace header', () => {
     let state = Plain.deserialize("# some text");
     const wrapper = shallow(<HeaderThreeButton state={state} onChange={(newState) => { state = newState }} />);
-    wrapper.find('button').simulate('click');
+    wrapper.simulate('click');
     expect(Plain.serialize(state)).to.equal('### some text');
   });
 });
