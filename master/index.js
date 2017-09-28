c9f09f8a6f52c6aeb4b7880002915cb0d55627d5
ocmachineuser <pim.machineuser@opuscapita.com>
2017-09-28 09:06:10 +0000

    Deploy to GitHub pages [ci skip]

--- master/index.js
+++ master/index.js
@@ -22805,7 +22805,7 @@ module.exports = [
   {
     "package": "@opuscapita/react-markdown",
     "name": "MarkdownInput",
-    "version": "1.1.1",
+    "version": "1.1.2",
     "tags": "",
     "relatedFiles": [
       {
@@ -22820,7 +22820,7 @@ module.exports = [
   {
     "package": "@opuscapita/react-markdown",
     "name": "PlainMarkdownInput",
-    "version": "1.1.1",
+    "version": "1.1.2",
     "tags": "",
     "relatedFiles": [
       {
@@ -22841,7 +22841,7 @@ module.exports = [
 module.exports = [
     {
         "name": "@opuscapita/react-markdown",
-        "version": "1.1.1",
+        "version": "1.1.2",
         "description": "React markdown editor component",
         "scripts": {
             "link-mode": "cross-env NODE_ENV=link webpack --config ./config/webpack.config.js",
