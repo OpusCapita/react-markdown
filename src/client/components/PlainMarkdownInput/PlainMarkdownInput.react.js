20534a89bc8fd5041a8bf5414cfe4712843cabeb
Aleksandr Baliunov <abaliunov-sc@users.noreply.github.com>
2017-10-05 14:38:17 +0300

    Bug/copy paste/#46 (#60)
    * Fix bug paste
    
    * Fix bug paste
    
    * Fix bug copy
    
    * Extract _getCopyText(state)
    
    * Fix bug with IE11
    
    * Add tests for multiline copy/paste events

--- src/client/components/PlainMarkdownInput/PlainMarkdownInput.react.js
+++ src/client/components/PlainMarkdownInput/PlainMarkdownInput.react.js
@@ -31,6 +31,27 @@ import {
 import { SlateContent, SlateEditor, SlateToolbar, SlateToolbarGroup } from '../SlateEditor';
 import Plain from 'slate-plain-serializer';
 
+function getCopyText(state) {
+  const { startKey, startOffset, endKey, endOffset, texts } = state;
+  let resText;
+
+  if (startKey === endKey) {
+    resText = texts.get(0).text.slice(startOffset, endOffset);
+  } else {
+    let resTextArr = texts.map((el, ind) => {
+      if (ind === 0) {
+        return el.text.slice(startOffset);
+      } else if (ind === texts.size - 1) {
+        return el.text.slice(0, endOffset);
+      } else {
+        return el.text;
+      }
+    });
+    resText = resTextArr.join('\n');
+  }
+  return resText;
+}
+
 class PlainMarkdownInput extends React.Component {
   state = {
     editorState: '',
@@ -116,6 +137,18 @@ class PlainMarkdownInput extends React.Component {
     return shortcuts(event, data, state);
   }
 
+  handleCopy(event, data, change) {
+    event.preventDefault();
+    const { state } = change;
+    const resText = getCopyText(state);
+    if (window.clipboardData) {
+      window.clipboardData.setData("Text", resText);
+    } else {
+      event.clipboardData.setData('text/plain', resText);
+    }
+    return change;
+  }
+
   render() {
     const { editorState, fullScreen } = this.state;
     const { children, extensions, readOnly } = this.props;
@@ -136,6 +169,7 @@ class PlainMarkdownInput extends React.Component {
         fullScreen={fullScreen}
         schema={schema}
         onChange={this.handleChange}
+        onCopy={this.handleCopy}
         plugins={[
           AutocompletePlugin({ extensions: extensions, onChange: this.handleChange })
         ]}
