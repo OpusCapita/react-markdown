0b0ea9067ed1f5195206df1421d246e15feae2ef
baliunov <baliunov@scand.com>
2017-10-06 10:14:56 +0300

    Refactoring

--- src/client/components/PlainMarkdownInput/slate/shortcuts.js
+++ src/client/components/PlainMarkdownInput/slate/shortcuts.js
@@ -9,68 +9,20 @@ import {
   wrapStrikethroughMarkdown,
   unwrapStrikethroughMarkdown,
 } from './transforms';
-import { getSlateEditor } from '../../SlateEditor/Utils';
-
-// this code will use in tomorrow
-// import hasUnorderedListMarkdown from './transforms';
-// import hasOrderedListMarkdown from './transforms';
-
-// const emptyListItem = /^[0-9]+\.\s$/;
-// const numbersFromListItem = /^[0-9]+/;
-
-const scrollAmount = 40;
+import { autoScrollToTop } from '../Utils';
 
 /**
  * Create new list item block, when the cursor move to a next line
  */
-// const enterKeyDownHandler = (event, data, state, editor) => {
 const enterKeyDownHandler = (event) => {
   if (event.keyCode === 13) {
-    let selection = window.getSelection();
-    let offsetTop = selection.anchorNode.parentNode.offsetTop;
-
-    let editor = getSlateEditor(selection);
-
-    if (editor.scrollTop + editor.offsetHeight < offsetTop + scrollAmount) {
-      editor.scrollTop = offsetTop - editor.offsetHeight + scrollAmount;
-    }
-
-    // this code will use in tomorrow
-    // if (hasUnorderedListMarkdown(state)) {
-    //   event.preventDefault();
-    //
-    //   const t = state.transform();
-    //   const { focusText } = state;
-    //   const focusedText = focusText.text;
-    //   if (focusedText === '* ') {
-    //     t.removeTextByKey(focusText.key, 0, focusedText.length).splitBlock();
-    //   } else {
-    //     t.splitBlock().insertText('* ');
-    //   }
-    //   return t.focus().apply();
-    // } else if (hasOrderedListMarkdown(state)) {
-    //   event.preventDefault();
-    //
-    //   const t = state.transform();
-    //   const { focusText } = state;
-    //   const focusedText = focusText.text;
-    //
-    //   if (emptyListItem.test(focusedText)) {
-    //     t.removeTextByKey(focusText.key, 0, focusedText.length).splitBlock();
-    //   } else {
-    //     const number = parseInt(numbersFromListItem.exec(focusedText)[0], 0);
-    //     t.splitBlock().insertText(`${number + 1}. `);
-    //   }
-    //   return t.focus().apply();
-    // }
+    autoScrollToTop();
   }
 
   return undefined;
 };
 
-// export default (event, data, state, editor) => { // eslint-disable-line
-export default (event, data, state) => { // eslint-disable-line
-  // const result = enterKeyDownHandler(event, data, state, editor);
+export default (event, data, state) => {
   const result = enterKeyDownHandler(event);
 
   if (result) {
--- src/client/components/SlateEditor/Utils.js
+++ src/client/components/SlateEditor/Utils.js
@@ -12,24 +12,6 @@ const cloneElement = (children, props) => {
   }));
 };
 
-export const getClosestElemFromClass = (elem, className) => {
-  if (!elem.parentElement) {
-    return null;
-  }
-  if (elem.parentElement.classList.contains(className)) {
-    return elem.parentElement;
-  }
-  return getClosestElemFromClass(elem.parentElement, className)
-};
-
-export const getSlateEditor = selection => {
-  if (selection.anchorNode.parentNode && selection.anchorNode.parentNode.closest) {
-    // return selection.anchorNode.parentNode.closest('.react-markdown--slate-content__editor');
-    return selection.anchorNode.parentNode.closest('.react-markdown--slate-content');
-  }
-  return getClosestElemFromClass(selection.anchorNode.parentNode, 'react-markdown--slate-content');
-};
-
 
 export default {
   cloneElement,
