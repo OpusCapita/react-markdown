d547b3b2c5ae91e0f494f88b0dd741c6c0f61a17
baliunov <baliunov@scand.com>
2017-10-13 13:11:21 +0300

    Remove old files

--- src/client/components/SlateEditor/SlateToolbar_.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Utils from './Utils';
-import './SlateToolbar.less';
-
-export default ({ children, ...rest }) => (
-  <div className="react-markdown--toolbar">
-    {Utils.cloneElement(children, rest)}
-  </div>
-);
