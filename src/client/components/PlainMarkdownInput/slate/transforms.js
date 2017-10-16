21d5de6d3076b1f45789ea8490ad7ea59467fb58
baliunov <baliunov@scand.com>
2017-10-16 16:33:49 +0300

    Remove old code

--- src/client/components/PlainMarkdownInput/slate/transforms.js
+++ src/client/components/PlainMarkdownInput/slate/transforms.js
@@ -33,17 +33,6 @@ const unwrapBlock = function(removedLength, state) {
   const endPos = Math.max(endOffset - removedLength, 0);
   change.moveOffsetsTo(0).deleteForward(removedLength).moveOffsetsTo(startPos, endPos).focus();
   return change.state;
-
-  // return state.transform().
-  //   moveOffsetsTo(0).
-  //   deleteForward(removedLength).
-  //   moveOffsetsTo(Math.max(startOffset - removedLength, 0), Math.max(endOffset - removedLength, 0)).
-  //   focus().apply();
-  // return state.transform().
-  //   moveOffsetsTo(0).
-  //   deleteForward(removedLength).
-  //   moveOffsetsTo(Math.max(startOffset - removedLength, 0), Math.max(endOffset - removedLength, 0)).
-  //   focus().apply();
 };
 
 /**
@@ -72,20 +61,6 @@ const wrapBlock = function(matchRules, text, state) {
   change.insertText(text).moveOffsetsTo(startPos, endPos).focus();
 
   return change.state;
-
-  // const t = state.transform().moveOffsetsTo(0);
-  // let length = 0;
-  // for (let i = 0, k = matchRules.length; i < k; i++) {
-  //   const result = matchRules[i].exec(focusedText);
-  //   if (result) {
-  //     length = result[0].length;
-  //     t.deleteForward(length);
-  //     break;
-  //   }
-  // }
-  // return t.insertText(text).
-  //   moveOffsetsTo(Math.max(startOffset + text.length - length, 0), Math.max(endOffset + text.length - length, 0)).
-  //   focus().apply();
 };
 
 /**
@@ -105,54 +80,6 @@ export const hasItalicMarkdown = state => {
 };
 
 /**
- * Wrap text italic with markdown token
- *
- * @param state - editor state
- */
-// export const wrapItalicMarkdown = state => {
-//   const { startOffset, endOffset } = state;
-//   const change = state.change();
-//   if (startOffset === endOffset) {
-//     const text = '';
-//     change.insertText('_' + text + '_').
-//       move(-1).
-//       extend(text.length * -1);
-//   } else {
-//     change.wrapText('_', '_');
-//   }
-//   change.focus();
-//   return change.state;
-//
-//   // let t = state.transform();
-//   // if (startOffset === endOffset) {
-//   //   const text = '';
-//   //   t.insertText('_' + text + '_').
-//   //     move(-1).
-//   //     extend(text.length * -1);
-//   // } else {
-//   //   t.wrapText('_', '_');
-//   // }
-//   // return t.focus().apply();
-// };
-
-/**
- * Unwrap text with italic markdown token
- *
- * @param state - editor state
- */
-// export const unwrapItalicMarkdown = state => {
-//   const { startOffset, endOffset, focusText } = state;
-//   const change = state.change();
-//   const focusKey = focusText.key;
-//   change.removeTextByKey(focusKey, endOffset, 1).removeTextByKey(focusKey, startOffset - 1, 1).focus();
-//   return change.state;
-//   // return state.transform().
-//   //   removeTextByKey(focusText.key, endOffset, 1).
-//   //   removeTextByKey(focusText.key, startOffset - 1, 1).
-//   //   focus().apply();
-// };
-
-/**
  * Has text wrapped with bold markdown tokens
  *
  * @param state - editor state
@@ -167,71 +94,6 @@ export const hasBoldMarkdown = state => {
     return false;
   }
 };
-
-/**
- * Wrap text with bold markdown tokens
- *
- * @param state - editor state
- */
-// export const wrapBoldMarkdown = state => {
-//   const { startOffset, endOffset } = state;
-//   const change = state.change();
-//   if (startOffset === endOffset) {
-//     const text = '';
-//     change.insertText('**' + text + '**').
-//     move(-2).
-//     extend(text.length * -1);
-//   } else {
-//     change.wrapText('**', '**');
-//   }
-//   change.focus();
-//   return change.state;
-//
-//   // let t = state.transform();
-//   // if (startOffset === endOffset) {
-//   //   const text = '';
-//   //   t.insertText('**' + text + '**').
-//   //     move(-2).
-//   //     extend(text.length * -1);
-//   // } else {
-//   //   t.wrapText('**', '**');
-//   // }
-//   // return t.focus().apply();
-// };
-
-/**
- * Unwrap text from accent
- *
- * @param state - editor state
- * @param count - count of the deleted characters
- */
-function unwrapLetters(count, state) {
-  const { startOffset, endOffset, focusText } = state;
-  const change = state.change();
-  const focusKey = focusText.key;
-  change.removeTextByKey(focusKey, endOffset, count).
-  removeTextByKey(focusKey, startOffset - count, count).focus();
-  return change.state;
-}
-
-/**
- * Unwrap text with bold markdown tokens
- *
- * @param state - editor state
- */
-// export const unwrapBoldMarkdown = state => {
-//   return unwrapLetters(state, 2);
-//   const { startOffset, endOffset, focusText } = state;
-//   const change = state.change();
-//   const focusKey = focusText.key;
-//   change.removeTextByKey(focusKey, endOffset, 2).removeTextByKey(focusKey, startOffset - 2, 2).focus();
-//   return change.state;
-//
-//   // return state.transform().
-//   //   removeTextByKey(focusText.key, endOffset, 2).
-//   //   removeTextByKey(focusText.key, startOffset - 2, 2).
-//   //   focus().apply();
-// };
 
 /**
  * Has wrap text with strikethrough markdown tokens
@@ -272,36 +134,19 @@ function wrapLetters(mark, state) {
 }
 
 /**
- * Wrap text with strikethrough markdown tokens
- *
- * @param state
- */
-// export const wrapStrikethroughMarkdown = state => {
-//   const { startOffset, endOffset } = state;
-//   let t = state.transform();
-//   if (startOffset === endOffset) {
-//     const text = '';
-//     t.insertText('~~' + text + '~~').
-//       move(-2).
-//       extend(text.length * -1)
-//   } else {
-//     t.wrapText('~~', '~~');
-//   }
-//   return t.focus().apply();
-// };
-
-/**
- * Unwrap text with strikethrought markdown tokens
+ * Unwrap text from accent
  *
  * @param state - editor state
+ * @param count - count of the deleted characters
  */
-// export const unwrapStrikethroughMarkdown = state => {
-//   const { startOffset, endOffset, focusText } = state;
-//   return state.transform().
-//     removeTextByKey(focusText.key, endOffset, 2).
-//     removeTextByKey(focusText.key, startOffset - 2, 2).
-//     focus().apply()
-// };
+function unwrapLetters(count, state) {
+  const { startOffset, endOffset, focusText } = state;
+  const change = state.change();
+  const focusKey = focusText.key;
+  change.removeTextByKey(focusKey, endOffset, count).
+  removeTextByKey(focusKey, startOffset - count, count).focus();
+  return change.state;
+}
 
 /**
  * Unwrap text with OL markdown token
@@ -360,9 +205,6 @@ const activities = {
     ]
   },
   unwrap: {
-    // bold: unwrapBoldMarkdown,
-    // italic: unwrapItalicMarkdown,
-    // strikethrough: unwrapStrikethroughMarkdown,
     bold: unwrapLetters.bind(null, 2),
     italic: unwrapLetters.bind(null, 1),
     strikethrough: unwrapLetters.bind(null, 2),
@@ -379,9 +221,6 @@ const activities = {
     ]
   },
   wrap: {
-    // bold: wrapBoldMarkdown,
-    // italic: wrapItalicMarkdown,
-    // strikethrough: wrapStrikethroughMarkdown,
     bold: wrapLetters.bind(null, '**'),
     italic: wrapLetters.bind(null, '_'),
     strikethrough: wrapLetters.bind(null, '~~'),
