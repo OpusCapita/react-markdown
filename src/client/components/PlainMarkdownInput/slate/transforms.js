d2a1e08e6d3ccf03df72a13072847917ddf2d7ce
baliunov <baliunov@scand.com>
2017-10-25 14:45:22 +0300

    Tmp commit

--- src/client/components/PlainMarkdownInput/slate/transforms.js
+++ src/client/components/PlainMarkdownInput/slate/transforms.js
@@ -28,6 +28,12 @@ const EMPHASIS = {
   strikethrough: '~~',
 };
 
+const EMPHASIS_ALL = {
+  bold: ['**', '__'],
+  italic: ['_', '*'],
+  strikethrough: ['~~'],
+};
+
 /**
  * Has block selected
  *
@@ -160,11 +166,11 @@ function hasEmphasis(mark, state) {
           // between italic markers and not bold's edge
           } else if (mark === 'italic' && prevChar.text === currChar.text &&
             (currChar.text === '_' || currChar.text === '*') && !hasPrevMark && !hasCurrMark &&
-              !(hasMarkOnChar(prevChar, 'bold') && hasMarkOnChar(currChar, 'bold') &&
+              (!(hasMarkOnChar(prevChar, 'bold') && hasMarkOnChar(currChar, 'bold')) &&
                 (startOffset === 1 || startOffset === focusText.length - 1 ||
                   (startOffset > 1 && !hasMarkOnChar(characters.get(startOffset - 2), 'bold'))) ||
                   (startOffset < focusText.length - 1 &&
-                   !hasMarkOnChar(characters.get(startOffset + 1), 'bold')))) {
+                    !hasMarkOnChar(characters.get(startOffset + 1), 'bold')))) {
             return true;
           }
         }
@@ -222,18 +228,99 @@ function wrapEmphasis(mark, state) {
  * @param state - editor state
  */
 function unwrapEmphasis(accent, state) {
-  let count = EMPHASIS[accent].length;
-  // 1) если выделение по маркерам, удаляем маркеры
-  // 2) если без выделения курсор обернут в маркеры, удаляем маркеры
+  let markerLength = EMPHASIS[accent].length;
+  // 1) если без выделения курсор обернут в маркеры, удаляем маркеры
+  // 2) если без выделения на маркере, ничего не делаем
   // 3) если без выделения внутри маркеров, добавляем маркеры на место курсора
-  // 4) если одна сторона выделения на маркере, а другая внутри маркеров,
+  // 4) если выделение по маркерам, удаляем маркеры
+  // 5) если одна сторона выделения на маркере, а другая внутри маркеров,
   //    снимаем выделение с части строки - переносим маркер, на котором находится край выделения, внутрь
-  const { startOffset, endOffset, focusText } = state;
+  const { startOffset, endOffset, focusText, texts } = state;
   const change = state.change();
   const focusKey = focusText.key;
-  change.removeTextByKey(focusKey, endOffset, count).
-  removeTextByKey(focusKey, startOffset - count, count).focus();
-  return change.state;
+  let characters = texts.get(0).charsData.characters;
+  const { text } = focusText;
+  const leftEdge = getLeftEdge(accent, characters, startOffset);
+  const rightEdge = getRightEdge(accent, characters, startOffset, text.length);
+  console.log(`startOffset: ${startOffset}`);
+  console.log(`endOffset: ${endOffset}`);
+  console.log(`leftEdge: ${leftEdge}`);
+  console.log(`rightEdge: ${rightEdge}`);
+
+  // no selection
+  if (startOffset === endOffset) {
+    if (startOffset > markerLength - 1 && startOffset < text.length - markerLength + 1) {
+      let leftMarker = text.substr(startOffset - markerLength, markerLength);
+      let rightMarker = text.substr(startOffset, markerLength);
+      // #1 the cursor is wrapped in markers, delete markers
+      if (leftMarker === rightMarker && EMPHASIS_ALL[accent].indexOf(leftMarker) !== -1) {
+        change.removeTextByKey(focusKey, endOffset, markerLength).
+        removeTextByKey(focusKey, startOffset - markerLength, markerLength).focus();
+        return change.state;
+      }
+    }
+
+    // #2 the cursor on a marker, do nothing
+    if (leftEdge + 1 === startOffset || leftEdge + markerLength === startOffset ||
+      rightEdge === startOffset || rightEdge - markerLength + 1 === startOffset) {
+      return state
+    // #3 the between markers, add markers to the place of the cursor
+    } else {
+      change.insertText(`${EMPHASIS[accent]}${EMPHASIS[accent]}`).
+      move(-markerLength).focus();
+      return change.state;
+    }
+
+  // selection
+  } else {
+    // the selection on the left edge, do nothing
+    if ((leftEdge === startOffset || leftEdge + 1 === startOffset) &&
+      leftEdge + 1 === endOffset || leftEdge + markerLength === endOffset) {
+      return state
+    }
+    // the selection on the right edge, do nothing
+    if ((rightEdge - markerLength === startOffset || rightEdge - markerLength + 1 === startOffset) &&
+      (rightEdge - 1 === endOffset || rightEdge === endOffset)) {
+      return state
+    }
+    // the selection between markers
+    if (leftEdge + markerLength < startOffset && endOffset < rightEdge - markerLength - 1) {
+      change.wrapText(EMPHASIS[accent], EMPHASIS[accent]).focus();
+      return change.state;
+    }
+    // the selection on markers, delete markers
+    if ((leftEdge <= startOffset || startOffset <= leftEdge + markerLength) &&
+      (rightEdge - markerLength <= endOffset || endOffset <= rightEdge)) {
+      change.removeTextByKey(focusKey, rightEdge - markerLength, markerLength).
+      removeTextByKey(focusKey, leftEdge, markerLength).focus();
+      return change.state;
+    }
+
+  }
+
+  return state;
+}
+
+function getLeftEdge(accent, characters, startPos) {
+  let leftEdge = -1;
+  for (let i = startPos - 1; i >= 0; i--) {
+    if (!hasMarkOnChar(characters.get(i), accent)) {
+      leftEdge = i + 1;
+      break
+    }
+  }
+  return leftEdge < 0 ? 0 : leftEdge;
+}
+
+function getRightEdge(accent, characters, startPos, maxPos) {
+  let rightEdge = -1;
+  for (let i = startPos + 1; i < maxPos; i++) {
+    if (!hasMarkOnChar(characters.get(i), accent)) {
+      rightEdge = i - 1;
+      break
+    }
+  }
+  return rightEdge < 0 ? maxPos : rightEdge;
 }
 
 /**
